1f1733a1e74265aeb6af29952f5d71547f62ae5b
jingwen kuang <jwkwendy@gmail.com>
2021-02-25 22:30:25 -0800

    Practice leetcode 200

--- 200 - Number of islands.js
+++ 200 - Number of islands.js
@@ -12,5 +12,36 @@
 // Output: 1
 
 var numIslands = function(grid) {
-    
+    let m = grid.length; 
+    let n = grid[0].length;
+    let count = 0;
+
+    var dfs = function(x, y){
+      if (x >= 0 && y >= 0 && x < m && y < n && grid[x][y] === '1') {
+        grid[x][y] = '0';
+        dfs(x + 1, y);
+        dfs(x, y + 1);
+        dfs(x - 1, y);
+        dfs(x, y - 1);
+      }
+    }
+
+    for (let i = 0; i < m; i++) {
+      for (let j = 0; j < n; j++) {
+        if (grid[i][j] === '1'){
+          count ++;
+          dfs(i, j);
+        }
+      }
+    }
+    return count;
 };
+
+let grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+
+console.log(numIslands(grid))
