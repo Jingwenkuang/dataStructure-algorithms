d0f038da6207042c7b50925c827efa16c3d08fbe
jingwen kuang <jwkwendy@gmail.com>
2021-03-11 00:33:58 -0800

    Practice binary merge tree

--- 617-MergeTwoBinaryTrees.js
+++ 617-MergeTwoBinaryTrees.js
@@ -17,3 +17,14 @@ Example 2:
 
 Input: root1 = [1], root2 = [1,2]
 Output: [2,2]
+
+function mergeTrees (root1, root2) {
+
+  if (root1 === null) return root2;
+  if (root2 === null) return root1;
+
+  root1.val += root2.val;
+  root1.left = mergeTrees(root1.left, root2.left);
+  root1.right = mergeTrees(root1.right, root2.right);
+  return root1;
+}
