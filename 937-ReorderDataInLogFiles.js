a69e5aa822857b166993c6760c8233523737177e
jingwen kuang <jwkwendy@gmail.com>
2021-03-14 23:56:03 -0700

    sorting

--- 937-ReorderDataInLogFiles.js
+++ 937-ReorderDataInLogFiles.js
@@ -24,3 +24,51 @@ Example 2:
 
 Input: logs = ["a1 9 2 3 1","g1 act car","zo4 4 7","ab1 off key dog","a8 act zoo"]
 Output: ["g1 act car","a8 act zoo","ab1 off key dog","a1 9 2 3 1","zo4 4 7"]
+
+
+var reorderLogFiles = function(logs) {
+  // Init letters as empty array
+  let letters = [];
+  // Init digits as empty array
+  let digits = [];
+
+  // Iterate through logs
+  for (let log of logs) {
+    // Split curr log by spaces and save as currLog
+    const currLog = log.split(" ");
+    // If currLog[1] is type number
+    if (!isNaN(parseInt(currLog[1]))) {
+      // Push curr log to digits
+      digits.push(log);
+    } else { // Else
+      // Push curr log to letters
+      letters.push(log);
+    }
+  }
+  
+  // Sort letterLogs
+  letters.sort(sortLog);
+  // Return letters concat digits
+  return letters.concat(digits);
+};
+
+
+var sortLog = (a, b, i = 1) => {
+  const arrA = a.split(" ");
+  const arrB = b.split(" ");
+  // If we reach the end of both logs
+  if (i === arrA.length && i === arrB.length) {
+    // Need to compare the identifiers
+    return arrA[0] < arrB[0] ? -1 : arrA[0] > arrB[0] ? 1 : 0;
+  }
+      
+  if (arrA[i] < arrB[i]) {
+    return -1;
+  } else if (arrA[i] > arrB[i]) {
+    return 1;
+  } else if (arrA[i] === arrB[i]) {
+    return sortLog(a, b, i + 1);
+  }
+
+  return 0;
+}
