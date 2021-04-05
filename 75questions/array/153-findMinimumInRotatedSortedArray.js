0db8faa2a3481ed3cbda403c04de96cb854eb2a8
jingwen kuang <jwkwendy@gmail.com>
2021-04-05 02:03:17 -0700

    Binary search

--- 75questions/array/153-findMinimumInRotatedSortedArray.js
+++ 75questions/array/153-findMinimumInRotatedSortedArray.js
@@ -27,6 +27,26 @@ Output: 11
 Explanation: The original array was [11,13,15,17] and it was rotated 4 times. 
 */
 
+
+//binary search time o(log(n)), space o(1)
 var findMin = function(nums) {
-    
+    if (nums.length === 1) return nums[0];
+    let left = 0;
+    let right = nums.length -1;
+
+    if (nums[right] > nums[0]) return nums[0]; //sorted in order
+
+    while (right >= left) {
+      let mid = Math.floor((left + right) / 2);
+      if (nums[mid] > nums[mid + 1]) return nums[mid + 1]; //next num is the smallest
+      if (nums[mid] < nums[mid - 1]) return nums[mid]; //previous nums is the smallest
+      if (nums[mid] > nums[0]) { //on the right side
+        left = mid + 1;
+      } else { //on the left side
+        right = mid - 1;
+      }
+    }
 };
+
+let nums = [4,5,6,7,0,1,2];
+console.log(findMin(nums))
