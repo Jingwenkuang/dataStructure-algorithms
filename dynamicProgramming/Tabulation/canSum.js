3f81dccb69b871565376616528494ceedcf86d5f
jingwen kuang <jwkwendy@gmail.com>
2021-04-09 23:57:40 -0700

    tabulation

---
+++ dynamicProgramming/Tabulation/canSum.js
@@ -0,0 +1,11 @@
+/* Write a function 'canSum(targetSum, numbers)' that takes in a targetSum 
+and an array of numbers as arguments.
+The function should return a boolean indicating whether or not it is possible
+to generate the targetSum using numbers from the array.
+You may use an element of the array as many times as needed.
+You may assume that all input numbers are nonnegative.
+*/
+
+
+console.log(canSum(7, [2, 3]))
+console.log(canSum(300, [7, 14]))
