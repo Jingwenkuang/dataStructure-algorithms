85269156052b9ada164b2c19cb25e1e73e5e11e6
jingwen kuang <jwkwendy@gmail.com>
2021-05-13 00:01:51 -0700

    leetcode

--- 238-productOfArrayExceptSelf.js
+++ 238-productOfArrayExceptSelf.js
@@ -14,3 +14,34 @@ Example 2:
 Input: nums = [-1,1,0,-3,3]
 Output: [0,0,9,0,0]
 */
+
+//time o(n), space o(n)
+var productExceptSelf = function(nums) {
+  let leftPro = 1;
+  let left = [1]
+  for(let i = 1; i < nums.length; i++) {
+    leftPro *= nums[i - 1];
+    left.push(leftPro);
+    // console.log(left)
+    //[ 1, 1, 2, 6 ]
+  }
+
+  let rightPro = 1;
+  let right = [1];
+  for (let j = nums.length - 2; j >= 0; j--) {
+    rightPro *= nums[j + 1];
+    right.unshift(rightPro);
+    console.log(right) 
+    //[ 24, 12, 4, 1 ]
+  }
+
+  let product = []
+  for (let k = 0; k < left.length; k++) {
+    let leftNum = left[k];
+    product.push(leftNum * right[k])
+  }
+
+  return product;
+}
+// console.log(productExceptSelf([1,2,3,4])) //[24,12,8,6]
+console.log(productExceptSelf([-1,1,0,-3,3])) //[0,0,9,0,0]
