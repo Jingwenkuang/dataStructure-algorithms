d5f50ca56fa0862feb603936b9b873b72d347e86
jingwen kuang <jwkwendy@gmail.com>
2021-05-14 23:56:32 -0700

    matrix problem

---
+++ 75questions/matrix/73-setMatrixZeros.js
@@ -0,0 +1,18 @@
+/*
+73. Set Matrix Zeroes
+Given an m x n matrix. If an element is 0, set its entire row and column to 0. Do it in-place.
+
+Follow up:
+
+A straight forward solution using O(mn) space is probably a bad idea.
+A simple improvement uses O(m + n) space, but still not the best solution.
+Could you devise a constant space solution?
+Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+Output: [[1,0,1],[0,0,0],[1,0,1]]
+
+Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+*/
+var setZeroes = function(matrix) {
+    
+};
