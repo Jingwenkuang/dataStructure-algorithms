b12f9681cbd07a6da5e9dede392f6d61e20c5c08
jingwen kuang <jwkwendy@gmail.com>
2021-05-15 23:35:35 -0700

    matrix

--- 75questions/matrix/73-setMatrixZeros.js
+++ 75questions/matrix/73-setMatrixZeros.js
@@ -14,5 +14,21 @@ Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
 Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
 */
 var setZeroes = function(matrix) {
+     let col0 = 1, row = matrix.length, col = matrix[0].length;
     
+    for(let i = 0; i < row; i++) {
+        if(matrix[i][0] === 0) col0 = 0;
+        for(let j = 1; j < col; j++) {
+            if(matrix[i][j] === 0) {
+                matrix[i][0] =  matrix[0][j] = 0;
+            }
+        }
+    }
+    
+    for(let i = row-1; i >= 0; i--) {
+        for(let j = col-1; j > 0; j--) {
+            if(matrix[i][0] === 0 || matrix[0][j] === 0) matrix[i][j] = 0;
+        }
+        if(col0 === 0) matrix[i][0] = 0;
+    }   
 };
