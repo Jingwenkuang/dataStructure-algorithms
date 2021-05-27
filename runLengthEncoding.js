fbe69e176fcf68852a5c6d731b70f5343cb853f4
jingwen kuang <jwkwendy@gmail.com>
2021-05-27 16:09:14 -0700

    encoding

--- runLengthEncoding.js
+++ runLengthEncoding.js
@@ -5,20 +5,23 @@ ouput: '9A4A2B4C2D'
 
 function runLengthEncoding(string) {
   // Write your code here.
-	let count = 0; 
-	let newStr = "";
-	
-	for (let i = 1; i < string.length; i++) {
-		let current = string[i];
-		let next = string[i - 1];
-		if (current === next){
-			count += 1;
-			if (count >= 9) {
-				newStr += (count + current);
-				count = 1;
-				console.log(newStr)
-			}
-		}
-	}
-	return newStr;
+	let count = 1; 
+	let newStr = [];
+
+  for(let i = 1; i < string.length; i++){
+    let current = string[i];
+    let previous = string[i - 1];
+
+    if (current !== previous || count === 9) {
+      newStr.push(count.toString());
+      newStr.push(current);
+      count = 0; 
+    }
+    count += 1;
+  }
+  newStr.push(count.toString());
+  newStr.push(string[string.length - 1]);
+ 	return newStr.join('');
 }
+
+console.log(runLengthEncoding("AAAAAAAAAAAABBCCCCDD"));//9A3B2C4D2D
